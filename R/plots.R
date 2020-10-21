42531d7d59bbaaaff250321b5c096a7052d4b711
Hélène Langet <langhe@sti.local>
2020-10-21 21:20:58 +0200

    Add gauge plot

--- R/plots.R
+++ R/plots.R
@@ -22,6 +22,24 @@ generate_enrolment_hist <- function(data){
 
 }
 
+#' Plot enrolment gauge
+#'
+#' This function plots a gauge representing enrolment.
+#'
+#' Description of plot_enrolment_gauge
+#' @param m Maximal gauge value
+#' @param s Sucess
+#' @param w Warning
+#' @param d Danger
+#' @return This function plots a gauge representing enrolment.
+#' @export
+
+plot_enrolment_gauge <- function(m, s, w, d){
+
+  gauge(sum(data$x), min = 0, max = m, sectors = gaugeSectors(success = s, warning = w, danger = d))
+
+}
+
 #' Generate pie chart with label
 #'
 #' This function plots a pie chart.
