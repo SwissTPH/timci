2d7702f80467b03875427c3a1404b98aec986fe6
Hélène Langet <langhe@sti.local>
2021-07-18 14:36:51 +0200

    Update PATH M&E report

--- R/format_timci_fa_data.R
+++ R/format_timci_fa_data.R
@@ -28,31 +28,6 @@ extract_last_fa_data <- function(df, stats) {
 
   # Retrieve last assessment
   res <- plyr::ddply(df, 'fcode', head, 1)
-  res <- merge(x = res, y = stats, by.x = 'fcode', by.y = 'facility_id', all.x = TRUE)
-
-  # Retrieve previous assessment
-  prev <- plyr::ddply(df, 'fcode', head, 2)
-  prev <- plyr::ddply(prev, 'fcode', tail, 1)
-
-  # Add the date of the last assessment
-  res <- merge(x = res, y = prev[,c('date','fcode')], by = 'fcode', all.x = TRUE)
-  res %>% dplyr::rename('date' = 'date.x',
-                        'prev' = 'date.y')
-
-}
-
-#' Extract last available facility assessment data (TIMCI-specific function)
-#'
-#' @param df dataframe containing the processed facility assessment data
-#' @param stats dataframe containing summary statistics for each facility
-#' @return This function returns a dataframe that only contains the last available assessment for each facility.
-#' @export
-#' @import dplyr magrittr
-
-extract_last_fa_data2 <- function(df, stats) {
-
-  # Retrieve last assessment
-  res <- plyr::ddply(df, 'fcode', head, 1)
   res <- merge(x = res, y = stats, by.x = 'fcode', by.y = 'facility_id', all = TRUE)
 
   # Retrieve previous assessment
@@ -75,7 +50,7 @@ extract_last_fa_data2 <- function(df, stats) {
 
 display_weekly_fa_data_per_facility <- function(df, all_df) {
 
-  sub <- df[df$pox_in_use == 0,]
+  sub <- df[!is.na(df$facility) & df$pox_in_use == 0,]
   n <- nrow(sub)
   cat('##', n, 'facility(ies) where the pulse oximeter is not used\n\n')
   if (n > 0) {
@@ -85,7 +60,7 @@ display_weekly_fa_data_per_facility <- function(df, all_df) {
   } else {
     cat('N/A\n\n')
   }
-  sub <- df[df$hcp_arrival == 1,]
+  sub <- df[!is.na(df$facility) & df$hcp_arrival == 1,]
   n <- nrow(sub)
   cat('##', n, 'facility(ies) where a new provider started\n\n')
   if (n > 0) {
@@ -95,7 +70,7 @@ display_weekly_fa_data_per_facility <- function(df, all_df) {
   } else {
     cat('N/A\n\n')
   }
-  sub <- df[df$hcp_departure == 1,]
+  sub <- df[!is.na(df$facility) & df$hcp_departure == 1,]
   n <- nrow(sub)
   cat('##', n, 'facility(ies) where a trained provider stopped working\n\n')
   if (n > 0) {
@@ -109,32 +84,37 @@ display_weekly_fa_data_per_facility <- function(df, all_df) {
   for (i in 1:nrow(df)) {
     row <- df[i,]
     fid <- row$'fcode'
-    cat('###', sprintf("%s", row$facility),'\n\n')
-    cat('Recruitment covers the period from ', paste0('**', as.character(row$recruitment_start), '**'), ' to ' , paste0('**', as.character(row$recruitment_last), '**'),'\n\n')
-    cat('* ', paste0('**', row$children, '**'), 'children 0-59m enrolled\n\n')
-    cat('* ', paste0('**', row$female, '**'), 'female children 0-59m enrolled\n\n')
-    cat('* ', paste0('**', row$yg_infant, '**'), 'young infants (0-2m) enrolled\n\n')
-    cat('* ', paste0('**', row$yg_female, '**'), 'female young infants (0-2m) enrolled\n\n')
-    res <- all_df[all_df$fcode == fid,]
-    cat(paste0('**', nrow(res), '**'), ' weekly assessment(s) available for this facility\n\n')
-    cat('The latest assessment covers the period from ', paste0('**', as.character(row$prev), '**'), ' to ', paste0('**', as.character(row$date), '**'), '\n\n')
-    cat('* Pulse oximeter in use for the sick neonate / child consultations: ')
-    if (row$pox_in_use == 1) {
-      cat('**YES** \n\n')
-    } else {
-      cat('**NO** \n\n')
-    }
-    cat('* New provider started working at the facility: ')
-    if (row$hcp_arrival == 1) {
-      cat('**YES** \n\n')
-    } else {
-      cat('**NO** \n\n')
-    }
-    cat('* Trained provider stopped working at this facility: ')
-    if (row$hcp_departure == 1) {
-      cat('**YES** \n\n')
-    } else {
-      cat('**NO** \n\n')
+    if (!is.na(row$facility)) {
+      cat('###', sprintf("%s", row$facility),'\n\n')
+      cat('Recruitment covers the period from ', paste0('**', as.character(row$recruitment_start), '**'), ' to ' , paste0('**', as.character(row$recruitment_last), '**'),'\n\n')
+      cat('* ', paste0('**', ifelse(!is.na(row$screened), row$screened, 0), '**'), 'children screened\n\n')
+      cat('* ', paste0('**', ifelse(!is.na(row$children), row$children, 0), '**'), 'children 0-59m enrolled\n\n')
+      cat('* ', paste0('**', ifelse(!is.na(row$female), row$female, 0), '**'), 'female children 0-59m enrolled\n\n')
+      cat('* ', paste0('**', ifelse(!is.na(row$yg_infant), row$yg_infant, 0), '**'), 'young infants (0-2m) enrolled\n\n')
+      cat('* ', paste0('**', ifelse(!is.na(row$yg_female), row$yg_female, 0), '**'), 'female young infants (0-2m) enrolled\n\n')
+      res <- all_df[all_df$fcode == fid,]
+      if (nrow(res) > 0) {
+        cat(paste0('**', nrow(res), '**'), ' weekly assessment(s) available for this facility\n\n')
+        cat('The latest assessment covers the period from ', paste0('**', as.character(row$prev), '**'), ' to ', paste0('**', as.character(row$date), '**'), '\n\n')
+        cat('* Pulse oximeter in use for the sick neonate / child consultations: ')
+        if (row$pox_in_use == 1) {
+          cat('**YES** \n\n')
+        } else {
+          cat('**NO** \n\n')
+        }
+        cat('* New provider started working at the facility: ')
+        if (row$hcp_arrival == 1) {
+          cat('**YES** \n\n')
+        } else {
+          cat('**NO** \n\n')
+        }
+        cat('* Trained provider stopped working at this facility: ')
+        if (row$hcp_departure == 1) {
+          cat('**YES** \n\n')
+        } else {
+          cat('**NO** \n\n')
+        }
+      }
     }
   }
 
@@ -148,6 +128,9 @@ display_weekly_fa_data_per_facility <- function(df, all_df) {
 #' @import dplyr magrittr
 
 format_weekly_fa_data_for_export <- function(df) {
+
+  df$facility <- ifelse(!is.na(df$facility), df$facility, df$facility_name)
+  df$district <- ifelse(!is.na(df$district), df$district, df$lvl2)
 
   # Order columns
   col_order <- c('fcode',
