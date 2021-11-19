def9c63bfbc5d5131a71881fdc5624440d4dcff1
Thaliehln <helene.langet@swisstph.ch>
2021-11-19 12:10:46 +0100

    Add new function send_email_tls
    The function send_email_tls() requires emayili v0.6.5 to work correctly and uses port 587 (TLS).

--- R/send_email.R
+++ R/send_email.R
@@ -36,3 +36,42 @@ send_email <- function(study_type, dest, attachment_list) {
   }
 
 }
+
+#' Send email
+#'
+#' This function sends an automated email with the generated reports.
+#' The gmail() function is only available for emayili v0.6.5 and above.
+#' It uses by default port 587 (TLS), but the port argument can be specified (e.g., port 465 for SSL).
+#'
+#' @param study_type String containing the type of study to specific the email subject
+#' @param dest Vector of strings containing email addresses
+#' @param attachment_list Vector containing the paths to the files to be attached to the email
+#' @import magrittr
+#' @export
+
+send_email_tls <- function(study_type, dest, attachment_list) {
+
+  if (!requireNamespace("emayili", quietly = TRUE)) {
+    stop("Package \"emayili\" needed for this function to work. Please install it.",
+         call. = FALSE)
+  } else {
+    email <- emayili::envelope() %>%
+      emayili::from(Sys.getenv('EMAIL_UN')) %>%
+      emayili::to(dest) %>%
+      emayili::subject(paste0("TIMCI ", study_type, " reports - ", Sys.getenv('TIMCI_COUNTRY'), " - ", format(Sys.time(), '%Y-%m-%d'))) %>%
+      emayili::text(paste0("Dear all,\n\nPlease receive the TIMCI daily report for ", Sys.getenv('TIMCI_COUNTRY'), ".\n\nBest regards,\n\nThe TIMCI team\n\n[THIS IS AN AUTOMATED MESSAGE - PLEASE DO NOT REPLY DIRECTLY TO THIS EMAIL]"))
+
+    smtp <- emayili::gmail(username = Sys.getenv('EMAIL_UN'),
+                           password = Sys.getenv('EMAIL_PW'))
+
+    for (atch in attachment_list) {
+      if (file.exists(atch)) {
+        email <- email %>%
+          emayili::attachment(atch)
+      }
+    }
+
+    smtp(email, verbose = TRUE)
+  }
+
+}
