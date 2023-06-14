0c3f96059dd37fce152a9cdde78b48a80373e2a9
Thaliehln <helene.langet@swisstph.ch>
2023-06-14 17:51:47 +0200

    Update Day 7 flowchart

--- R/create_cleaning_flowcharts.R
+++ R/create_cleaning_flowcharts.R
@@ -362,6 +362,7 @@ create_repeat_qc_flowchart <- function(n_raw_repeat_records,
 #' @param n_drop_nonvalid_pid_records
 #' @param n_inconsistent_name_records
 #' @param n_edit_inconsistent_name_records
+#' @param n_drop_inconsistent_names_day7fu_records
 #' @param n_cleaned_allday7fu_records TBD
 #' @return This function returns a graph object
 #' @export
@@ -374,9 +375,10 @@ create_day7fu_qc_flowchart <- function(n_raw_allday7fu_records,
                                        n_drop_nonvalid_pid_records,
                                        n_inconsistent_name_records,
                                        n_edit_inconsistent_name_records,
+                                       n_drop_inconsistent_names_day7fu_records,
                                        n_cleaned_allday7fu_records) {
 
-  n_excluded <- n_after_lockdate_records + n_drop_nonvalid_pid_records
+  n_excluded <- n_after_lockdate_records + n_drop_nonvalid_pid_records + n_drop_inconsistent_names_day7fu_records
   n_auto_edited <- 0
   n_manual_edited <- n_edit_nonvalid_pid_records + n_edit_inconsistent_name_records
   n_informed <- 0
@@ -386,7 +388,7 @@ create_day7fu_qc_flowchart <- function(n_raw_allday7fu_records,
                   node [fontname = Helvetica, shape = rectangle, fixedsize = false, width = 1]
 
                   1 [label = 'Raw Day 7 follow-up records\n(N = %s)', shape = folder, style = filled, fillcolor = '#f79679']
-                  m1 [label = 'Excluded (N = %s)\n%s record(s) outside the lock date range\n%s record(s) with non-valid child IDs\nXX record(s) with non-consistent names']
+                  m1 [label = 'Excluded (N = %s)\n%s record(s) outside the lock date range\n%s record(s) with non-valid child IDs\n%s record(s) with non-consistent names']
                   m2 [label = 'Automatically edited (N = %s)']
                   m3 [label = 'Manually edited (N = %s)\n%s record(s) edited for non-valid child IDs (out of %s detected)\n%s record(s) edited for non-consistent names (out of %s detected)']
                   m4 [label = 'Other checks triggered (N = %s)']
@@ -415,6 +417,7 @@ create_day7fu_qc_flowchart <- function(n_raw_allday7fu_records,
                 n_excluded,
                 n_after_lockdate_records,
                 n_drop_nonvalid_pid_records,
+                n_drop_inconsistent_names_day7fu_records,
                 n_auto_edited,
                 n_manual_edited,
                 n_edit_nonvalid_pid_records,
